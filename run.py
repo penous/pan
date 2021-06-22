b13de6443029f231d7e87f8824b88d7a96bcd935
Tom Dirinck <tom.dirinck@gmail.com>
2021-06-22 18:49:36 +0200

    Added env var for flask command.

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from sandwich import app
+from sandwich import app, db
+from sandwich.models import User, Shop, Sandwich
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Shop': Shop, 'Sandwich': Sandwich}
